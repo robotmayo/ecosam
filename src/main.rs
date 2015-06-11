cc45d909973a895c118f0de261fabf8506ae6868
robotmayo <robotmayo@gmail.com>
2015-06-11 00:44:33 -0400

    tillles

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+pub struct Tile {
+    food: i32,
+    weather: i32
+}
+
 fn main() {
     let world = gen_world(5);
     for y in world{
         for x in y{
-            print!("{}", x);
+            print!("{} ", x.food);
         }
         println!("");
     }
 }
 
-fn gen_world(size: i32) -> Vec<Vec<i32>> {
+fn gen_world(size: i32) -> Vec<Vec<Tile>> {
     let mut outer_vec = vec![];
     for i in 1..size{
         let mut inner = vec![];
         for j in 1..size{
-            inner.push(0);
+            inner.push(Tile {food : 20, weather : 1});
         }
         outer_vec.push(inner)
     }
